d6531c1e06a6f92e42308e1f21b4f5849c91a1cc
bella13062013 <102339738+bella13062013@users.noreply.github.com>
2022-11-03 22:13:29 +0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿create proc [dbo].[SP_AuthoLogin]
+@Username nvarchar(20),
+@Password nvarchar(20)
+as
+begin
+    if exists (select * from dangnhap where tendangnhap = @Username and matKhau = @Password and IDper = 1)
+        select 1 as code
+    else if exists (select * from dangnhap where tendangnhap = @Username and matKhau = @Password and IDper = 0)
+        select 0 as code
+    else if exists(select * from dangnhap where tendangnhap = @Username and matKhau != @Password) 
+        select 2 as code
+    else select 3 as code
+end
